233469870654612979c9f35746978c578731eb76
Grzegorz <gregory.lukasiewicz@gmail.com>
2021-03-29 22:52:20 +0200

    Documentation

--- gnome_station_analysis/fit.py
+++ gnome_station_analysis/fit.py
@@ -4,6 +4,28 @@ from . import functions as func
 
 # params: freq scale and complex signal, returns guess initial params for the fit
 def guess_initial(freq, sig, phi = 0):
+    """Guessing initial parameters for fitting complex lorentzian function to the given data set.
+
+    Parameters
+    ----------
+    freq : array like
+        frequency scale in Hz (from measurement)
+    sig : complex array like
+        complex lorentzian signal (from measurement)
+    phi : float
+        initial guess for phase (optional)
+
+    Returns
+    -------
+    p0 : array
+        [f0_guess, A_guess, gamma_guess, phi_guess] - table of initial parameters for fitting functions.complex_lorentz() and functions.complex_lorentz_lin_back() to 'freq' and 'sig' data set.
+
+    Notes
+    -----
+    The method is used in fit.complex_lorentz() and fit.complex_lorentz_lin_back().
+    Please look at the code to understand the method. Note that it works correctly only for a good quality measurements (for example the lorentzian peak must be the largest value of the data set).
+
+    """
     R = np.abs(sig)
     ind_max = np.where(R == np.max(R))  # R peak
     f0_guess = np.abs(freq[ind_max])[0]
@@ -15,12 +37,59 @@ def guess_initial(freq, sig, phi = 0):
 
 # params: freq scale and complex signal, returns popt and pcov of the complex_lorentz function fit
 def complex_lorentz(freq, sig, phi = 0):
+    """Fitting functions.lorentz_functions() to the given data set.
+
+    Parameters
+    ----------
+    freq : array like
+        frequency scale in Hz (from measurement)
+    sig : complex array like
+        complex lorentzian signal (from measurement)
+    phi : float
+        initial guess for phase (optional)
+
+    Returns
+    -------
+    popt, pcov : arrays
+        popt = [f0_fit, A_fit, gamma_fit, phi_fit] and covariance matrix pcov
+
+    Notes
+    -----
+
+    Initial conditions are given by fit.guess_initial().
+
+    To obtain the error of a chosen parameter popt[i] please use np.sqrt(pcov[i][i]).
+
+    """
     p0 = guess_initial(freq, sig, phi)
     sig_vector = np.hstack([sig.real, sig.imag])
     popt, pcov = curve_fit(func.vec_model, freq, sig_vector, p0 = p0)
     return popt, pcov
 
 def complex_lorentz_lin_back(freq, sig, phi = 0):
+    """Fitting functions.complex_lorentz_lin_back() to the data set.
+
+    Parameters
+    ----------
+    freq : array like
+        frequency scale in Hz (from measurement)
+    sig : complex array like
+        complex lorentzian signal (from measurement)
+    phi : float
+        initial guess for phase (optional)
+
+    Returns
+    -------
+    popt, pcov : arrays
+        popt = [f0_fit, A_fit, gamma_fit, phi_fit, a_real_fit, b_real_fit, a_imag_fit, b_imag_fit] and covariance matrix pcov
+
+    Notes
+    -----
+    Initial conditions are given by fit.guess_initial().
+
+    To obtain the error of a chosen parameter popt[i] please use np.sqrt(pcov[i][i]).
+
+    """
     p0 = guess_initial(freq, sig, phi)
     p0 = p0 + [0,0,0,0] # adding initial params for liear background
     sig_vector = np.hstack([sig.real, sig.imag])
--- gnome_station_analysis/read.py
+++ gnome_station_analysis/read.py
@@ -5,6 +5,32 @@ import glob
 # returns single dimension tables
 # after file_name one specifies which colums of data should be returned (beginning with 0!)
 def file(file_name, *columns):
+    """Reads selected columns in a chosen file
+
+    Parameters
+    ----------
+    file_name : string
+    columns : int (multiple)
+        specifies wchich colums od data should be returned (beginning with 0!)
+
+    Returns
+    -------
+    (columns) : list of arrays
+        columns of data listed in parameters after file_name
+
+    Examples
+    --------
+
+    >time, Ch2 = gnome_station_analysis.read.file("name", 0, 2) \n
+    returns 2 arrays of data: time and signal in channel 2.
+
+    >col = [0, 2, 3] \n
+    >data = gnome_station_analysis.read.file("name", *col) \n
+    data[0] - 1st column of data \n
+    data[1] - 3rd column of data \n
+    data[2] - 4th column of data
+
+    """
     data = np.loadtxt(file_name)
     tab = []
     for i in columns:
@@ -13,12 +39,43 @@ def file(file_name, *columns):
 
 # returns table of file names in a given directory (not sorted!)
 def all_names(directory):
+    """Finding all .dat files in a given directory
+
+    Parameters
+    ----------
+    directory : string
+        path to a direcotry with a series of measurement files
+
+    Returns
+    -------
+        file_names : array of strings
+            all .dat files in a given directory
+
+    """
     if directory[-1] != '/':
         directory += '/'
     return np.array(glob.glob(directory + "*.dat"))
 
 # reads current value from file name there must be such a pattern in the file name: "Curr_iii_uA"
 def current(file_name):
+    """ Returns current value saved in the file name
+
+    Parameters
+    ----------
+    file_name : string
+        single file path
+
+    Returns
+    -------
+    curr : float
+        current value in uA
+
+    Notes
+    -----
+
+    The file name must be 'path.../name...Curr_iii_uA.dat'. The function reads 'iii' and returns the float value.
+
+    """
     current_reg_ex = re.compile(r'Curr_(-)*\d(\d)?(\d)?(\d)?(\d)?_uA')
     current_val = current_reg_ex.search(file_name)
     current_val_float = float(current_val.group().split('_')[1])
@@ -26,6 +83,18 @@ def current(file_name):
 
 # returns all current values (not sorted!)
 def all_currents(directory):
+    """Reads all currents saved in file names in a given direcotory
+
+    Parameters
+    ----------
+    directory : string
+
+    Returns
+    -------
+    currs : numpy array
+        values of current in measurements saved in the given directory
+
+    """
     file_names = all_names(directory)
     curr = []
     for file_name in file_names:
@@ -34,6 +103,21 @@ def all_currents(directory):
 
 # sorts the input tables by current values
 def sort_by_current(file_names, currents):
+    """Sorts both arrays according to rising current value
+
+    Parameters
+    ----------
+    file_names : array of strings
+        table of file names
+    currents : array of floats
+        array of current values corresponding to file_names
+
+    Returns
+    -------
+    file_names, currents : array of strings, array of floats
+        arrays sorted by rising current values
+
+    """
     currents = np.array(currents)
     inds = np.argsort(currents)
     currents = currents[inds]
@@ -42,6 +126,18 @@ def sort_by_current(file_names, currents):
 
 # returns currents and names sorted by current value
 def all_names_currents(directory):
+    """Reads all file_names and currents saved in file names in a given direcotory
+
+    Parameters
+    ----------
+    directory : string
+        path to a direcotry with a series of measurements
+
+    Returns
+    -------
+    file_names, currents : array of strings, array of floats
+        arrays sorted by rising current values
+    """
     names = all_names(directory)
     currs = all_currents(directory)
     return sort_by_current(names, currs)
@@ -51,6 +147,34 @@ def all_names_currents(directory):
 # a and b defines the interval in the time domain that is taken to calculate fft
 # fmin and fmax defines the interval in the frequency domain
 def comp_fft(time, time_sig, a = 0, b = -1, fmin = 0, fmax = 0):
+    """Computes FFT of real signal in time domain
+
+    Parameters
+    ----------
+    time : array
+        time scale in s
+    time_sig : array of real floats
+        array of real-valued signal in time domain
+    a : int
+        element of the time array defining the beginning of the interval that is going to be used in FFT (optional)
+    b : int
+        element of the time array defining the end of the interval that is going to be used in FFT (optional)
+    fmin : float
+        minimal value of the returned freq scale in Hz (optional)
+    fmax : float
+        maximal value of the returned freq scale in Hz (optional)
+        if fmax = 0 the maximal value of freq scale is taken (Nyquist frequency)
+
+    Returns
+    -------
+    freq, sig : array of floats, array of complex floats
+        frequency scale (only positive values) in Hz and complex signal in frequency domain
+
+    Notes
+    -----
+    The function uses numpy.fft.rfft. Please read numpy documentation for more details.
+
+    """
     fft = np.fft.rfft(time_sig[a:b])
     freq = np.fft.rfftfreq(len(time[a:b]), time[1]-time[0])
 
